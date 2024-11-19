d3a0afb9faf1bc0da84664c906b6cfe03f34e121
LTNhut <ltnhuta23035@cusc.ctu.edu.vn>
2024-11-19 08:21:58 +0700

    second commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://Boomie:2709@cluster0.jl09t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
